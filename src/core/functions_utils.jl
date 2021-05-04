5c88b975d46576a62980e0918eb48d652ac3ad8b
Yuchi Yamaguchi <abap0002@gmail.com>
2021-05-04 10:40:29 +0900

    Create functions_utils.jl

---
+++ src/core/functions_utils.jl
@@ -0,0 +1,18 @@
+get_values(var) = var isa Variable ? var.values : var
+
+
+function get_gys(f)
+    outputs = f.grad_field.outputs
+    return [output.grad for output in outputs]
+end
+
+function ones_like(x)
+    shape = size(x)
+    return isempty(shape) ? 1 : ones(shape)
+end
+
+function cleargrad!(var::Variable)
+    var.grad = nothing
+end
+
+
