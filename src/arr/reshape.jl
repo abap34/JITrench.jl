8d893c07bc2d0d24b96fa600ce5344c05c17132f
Yuchi Yamaguchi <abap0002@gmail.com>
2021-05-04 12:07:59 +0900

    Create reshape.jl

---
+++ src/arr/reshape.jl
@@ -0,0 +1,21 @@
+import Base
+
+
+mutable struct Reshape <: Functional
+    grad_field :: GradField
+    in_shape
+    out_shape
+    Reshape(shape) = new(GradField(), nothing, shape)
+end
+
+function forward(f::Reshape, x)
+    f.in_shape = size(x)
+    out = reshape(x, f.out_shape)
+    return [out]
+end
+
+function backward(f::Reshape, gys)
+    return reshape(gys, f.in_shape)
+end
+
+Base.reshape(x::Variable, shape) = Reshape(shape)(x)
