39d2e23c28f1d823ea34cd1bbb8cbf0d5a791aa8
Yuchi Yamaguchi <abap0002@gmail.com>
2021-06-06 22:34:55 +0900

    add min_len

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,13 +1,13 @@
 using Primes
 
-function randfact(N; max_len=10)
-    l = rand(1:max_len)
+function randfact(N; min_len=1, max_len=10)
+    l = rand(min_len:max_len)
     base_arr = factor(Array, N)
     ind = rand(1:l, length(base_arr))
     return prod.(getindex.(Ref(base_arr),　(i -> ind .== i).(1:l)))
 end
 
 
-function generate_shape(n_elemtnt; max_dim=5)
-    return Tuple(randfact(n_elemtnt, max_len=max_dim)), Tuple(randfact(n_elemtnt, max_len=max_dim))
+function generate_shape(n_elemtnt; min_dim=1, max_dim=5)
+    return Tuple(randfact(n_elemtnt, max_len=max_dim)), Tuple(randfact(n_elemtnt, min_len=min_dim, max_len=max_dim))
 end
