84aeef8957ad857f8c5c399d5da93a37ea562057
Yuchi Yamaguchi <abap0002@gmail.com>
2021-06-06 23:16:22 +0900

    move util function from runtests.jl to test_utils.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,63 +6,9 @@ using JITrench
 
 include("test_utils.jl")
 
-
-function generate_expr(N)
-    randop() = sample(["+", "-", "/", "*", "^"])
-    randnum(scale=100) = @sprintf("%.2f", scale * (rand() - 0.5))
-    randarg() = rand() > 0.5 ? "x" : randnum()
-    S = "$(randarg()) $(randop()) $(randarg())"
-    N = 10
-    for _ in 1:N
-        S = "($S) $(randop()) $(randarg())"
-    end
-    return S
-end
-
-
-function numerical_diff(f::Function, x::Real; e=1e-7)
-    return (f(x + e) - f(x - e)) / 2e
-end
-
-
-function numerical_diff(f::Function, xs::AbstractArray; e=1e-7)
-    grads = zeros(length(xs)) 
-    for idx in 1:length(xs)
-        tmp_val = xs[idx]
-        xs[idx] = tmp_val + e
-        fxh1 = f(xs...)
-        xs[idx] = tmp_val - e
-        fxh2 = f(xs...)
-        grads[idx] = (fxh1 - fxh2) / 2e
-        xs[idx] = tmp_val
-    end
-    return grads
-end
-
-
-function backward_diff(f::Function, xs::AbstractArray)
-    inputs = Variable.(xs)
-    outs = f(inputs...)
-    backward!(outs)
-    return (input -> input.grad.values).(inputs)
-end
-
-function isAbout(x, y; e=1e-4)
-    return ((x - y) < 1e-15) || (-e < ((x - y) / y) < e)
-end
-
-function isAbout(X::AbstractArray, Y::AbstractArray; e=1e-4)
-    for (x, y) in zip(X, Y)
-        if !(((x - y) < 1e-15) || (-e < ((x - y) / y) < e))
-            return false
-        end
-    end
-    return true
-end
-
 operators = [+, -, /, *, ^]
 
-N_TEST_COUNT = 1
+N_TEST_COUNT = 10
 
 @testset "OperatorTest" begin
     @testset "ForwardTest" begin
