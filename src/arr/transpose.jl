c717cac4df928b2a4042c75e62cb44618c75678d
Yuchi Yamaguchi <abap0002@gmail.com>
2021-06-06 23:19:43 +0900

    support vector

--- src/arr/transpose.jl
+++ src/arr/transpose.jl
@@ -1,15 +1,21 @@
 import Base
 
 mutable struct Transpose <: Functional
+    in_is_vector
     grad_field::GradField
+    Transpose(in_shape, grad_field) = new(length(in_shape) == 1, grad_field)
 end
 
 function forward(::Transpose, x)
-    return transpose(x)
+    return transpose(x) 
 end
 
-function backward(::Transpose, gys)
-    return transpose(gys)
+function backward(f::Transpose, gys)
+    gx = transpose(gys)
+    if f.in_is_vector
+        gx.values = gx.values[:]
+    end
+    return gx
 end
 
-Base.transpose(x::Variable) = Transpose(GradField())(x)
+Base.transpose(x::Variable) = Transpose(size(x.values), GradField())(x)
