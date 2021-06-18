daef53402463750bd3f6675dc986a830b1323eb7
Yuchi Yamaguchi <abap0002@gmail.com>
2021-06-19 01:07:18 +0900

    support sum_to

--- src/arr/sum_to.jl
+++ src/arr/sum_to.jl
@@ -8,10 +8,21 @@ end
 
 
 function _sum_to(x, shape)
+    if shape == ()
+        is_scalar = true
+        shape = (1, )
+    end
     x_shape = size(x)
     (shape == x_shape) && (return x)
     sum_axis = findall(x -> x != 0, shape .- x_shape) 
     result = _sum(x, dims=sum_axis)
+    if shape == (1, ) && is_scalar
+        if prod(size(result)) == 1
+            return result
+        else
+            throw(DimensionMismatch("fault sum_to. result: $result, result must be scalar."))
+        end
+    end
     if size(result) != shape
         throw(DimensionMismatch("fault sum_to. result: $result, shape:$shape"))
     else
