02551042548a2944fb713c081bc9d13f7e974529
Documenter.jl <documenter@juliadocs.github.io>
2021-07-08 11:47:59 +0000

    build based on 4e1d78f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JITrench","category":"page"},{"location":"#JITrench","page":"Home","title":"JITrench","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JITrench]","category":"page"}]
+}
