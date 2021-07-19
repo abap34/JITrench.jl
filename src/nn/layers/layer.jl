8c73aab054233769ae59b0c34baa7931356a1bbf
Yuchi Yamaguchi <abap0002@gmail.com>
2021-07-19 23:24:44 +0900

    better impl in parameters()

--- src/nn/layers/layer.jl
+++ src/nn/layers/layer.jl
@@ -8,14 +8,22 @@ function (layer::Layer)(x...)
 end
 
 
+function parameters(layer::Layer; with_value=true)
+    if with_value
+        return layer.param._dict
+    else
+        return keys(layer.param._dict)
+    end
+end
+
 function cleargrads!(layer::Layer)
-    for param in values(layer.param._dict)
+    for (_, param) in parameters(layer, with_value=true)
         cleargrad!(param)
     end
 end
 mutable struct Parameters
     _dict
-    Parameters() = new(Dict{Symbol, Variable}())
+    Parameters() = new(Dict{Symbol, Union{Nothing, Variable}}())
 end
 
 function Base.setproperty!(param::Parameters, name::Symbol, value) 
