ad3b91e1dd1d96b98aac889f9912263214cbd6d8
Yuchi Yamaguchi <abap0002@gmail.com>
2022-03-13 18:53:05 +0900

    fix bug in broadcast.jl

--- test/src/arr/broadcast.jl
+++ test/src/arr/broadcast.jl
@@ -15,25 +15,26 @@ broadcastable_functions =  [
 @testset "BroadCastForwardTest" begin
     for func in broadcastable_functions
         if func[2] == 2
-            # test args is scalar and vector
-            arg1 = randn()
-            arg2 = randn(50)
+            # test args are scalar and vector
+            arg1 = rand()
+            arg2 = rand(50)
             y = func[1].(arg1, arg2)
             y_jt = func[1].(Variable(arg1), Variable(arg2))
             @test y == y_jt.values
 
             # test args are vector
-            args = randn(100)
+            args = rand(100)
             arg1 = args[1:50]
             arg2 = args[51:100]
             y = func[1].(arg1, arg2)
             y_jt = func[1].(Variable(arg1), Variable(arg2))
             @test y == y_jt.values
+        else
+            arg = rand(50)
+            y = func[1].(arg)
+            y_jt = func[1].(Variable(arg))
+            @test y == y_jt.values
         end
     end
 end
 
-@testset "BroadCastBackwardTest" begin
-    for func in broadcastable_functions
-        if func
-end
