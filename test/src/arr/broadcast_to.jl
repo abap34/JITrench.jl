7ded68ac90f0258f109e08e734eba576a6af4206
Yuchi Yamaguchi <abap0002@gmail.com>
2022-04-03 00:28:36 +0900

    change to use @testset

--- test/src/arr/broadcast_to.jl
+++ test/src/arr/broadcast_to.jl
@@ -1,33 +1,39 @@
 using Test
 using JITrench
 
-x = Variable(1)
-
-y = JITrench.broadcast_to(x, (2, 2))
-
-@test y.values == [1 1; 1 1]
-@test y.creator isa JITrench.BroadcastTo
-@test y.creator.shape == (2, 2)
-@test y.creator.x_shape == () 
-
-backward!(y, retain_grad=true)
-
-@test size(y.grad) == size(y)
-@test size(x.grad) == size(x)
-@test y.grad.values == [1 1; 1 1]
-@test x.grad.values == 4
 
 
-x = Variable([1, 2])
+@testset "BroadCastToForwardTest" begin
 
-y = JITrench.broadcast_to(x, (2, 2))
+    x = Variable(1)
 
-@test y.values == [1 1; 2 2]
-@test y.creator isa JITrench.BroadcastTo
-@test y.creator.shape == (2, 2)
-@test y.creator.x_shape == (2, ) 
+    y = JITrench.broadcast_to(x, (2, 2))
 
-backward!(y, retain_grad=true)
+    @test y.values == [1 1; 1 1]
+    @test y.creator isa JITrench.BroadcastTo
+    @test y.creator.shape == (2, 2)
+    @test y.creator.x_shape == () 
 
-@test size(y.grad) == size(y)
-@test size(x.grad) == size(x)
+    backward!(y, retain_grad=true)
+
+    @test size(y.grad) == size(y)
+    @test size(x.grad) == size(x)
+    @test y.grad.values == [1 1; 1 1]
+    @test x.grad.values == 4
+
+
+    x = Variable([1, 2])
+
+    y = JITrench.broadcast_to(x, (2, 2))
+
+    @test y.values == [1 1; 2 2]
+    @test y.creator isa JITrench.BroadcastTo
+    @test y.creator.shape == (2, 2)
+    @test y.creator.x_shape == (2, ) 
+
+    backward!(y, retain_grad=true)
+
+    @test size(y.grad) == size(y)
+    @test size(x.grad) == size(x)
+
+end
