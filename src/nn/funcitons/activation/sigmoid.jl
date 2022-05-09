08a9bb91d897708a55a5a6dd1b0a2f7e8c643c71
Yuchi Yamaguchi <abap0002@gmail.com>
2022-05-09 23:27:14 +0900

    fix bug in `pure_func(::Sigmoid)`

--- src/nn/funcitons/activation/sigmoid.jl
+++ src/nn/funcitons/activation/sigmoid.jl
@@ -1,27 +1,27 @@
 mutable struct Sigmoid <: SingleReturnFunction
-    grad_field :: GradField
+    grad_field::GradField
 end
 
 
-function _sigmoid(x::T) where {T <: Real}
+function _sigmoid(x::T) where {T<:Real}
     one(T) / (one(T) + exp(-x))
 end
 
 @inline forward(::Sigmoid, x) = _sigmoid(x)
 
-function backward(f::Sigmoid, gy::Variable{T}) where {T <: Real}
+function backward(f::Sigmoid, gy::Variable{T}) where {T<:Real}
     y = f.grad_field.outputs[1]
     return (y * (1 - y)) * gy
 end
 
-function backward(f::Sigmoid, gy::Variable{T}) where {T <: AbstractArray}
+function backward(f::Sigmoid, gy::Variable{T}) where {T<:AbstractArray}
     y = f.grad_field.outputs[1]
     @. return (y * (1 - y)) * gy
 end
 
 sigmoid(x::Variable) = Sigmoid(GradField())(x)
-base_to_jt(::typeof(sigmoid)) = Sigmoid
-jt_to_base(::Sigmoid) = sigmoid
+jt_func(::typeof(sigmoid)) = Sigmoid
+pure_func(::Sigmoid) = _sigmoid
 
 
 
