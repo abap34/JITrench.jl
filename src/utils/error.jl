b13bf72c26e043f0da16c1bcb8a283695e5348c4
Yuchi Yamaguchi <abap0002@gmail.com>
2022-12-19 18:00:36 +0900

    implement `NotSameDeviceError`

--- src/utils/error.jl
+++ src/utils/error.jl
@@ -3,3 +3,25 @@ struct NotImplemetedError <: Exception
 end
 
 Base.showerror(io::IO, e::NotImplemetedError) = print(io, e.msg)
+
+struct NotSameDeviceError <: Exception
+    same_accelerator :: Bool
+    same_gpu_idx :: Bool
+    function NotSameDeviceError(;same_accelerator, same_gpu_idx)
+        if (same_accelerator) && (same_gpu_idx)
+            throw(DomainError("same_accelerator and same_gpu_idx can never be false at the same time"))
+        end
+        return new(same_accelerator, same_gpu_idx)
+    end
+end
+
+
+function Base.showerror(io::IO, e::NotSameDeviceError)
+    if !(same_accelerator)
+        print(io, "Arguments must be in the same device, Arguments are on both the CPU and the GPU.")
+    end
+
+    if !(same_gpu_idx)
+        print(io, "All arguments must be in the same device. Arguments are on different GPUs.")
+    end
+end
