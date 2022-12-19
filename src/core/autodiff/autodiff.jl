2c9b0150d5e11c2196d27f739b4d7763cf50a1fe
Yuchi Yamaguchi <abap0002@gmail.com>
2022-12-19 17:58:53 +0900

    rename functions

--- src/core/autodiff/autodiff.jl
+++ src/core/autodiff/autodiff.jl
@@ -1,13 +1,14 @@
 module AutoDiff
 
 abstract type Variable end
-abstract type DiffableFunction  end
+abstract type DiffableFunction end
 
 include("device.jl")
 include("variable.jl")
+include("function_utils.jl")
 include("function.jl")
 include("propagation.jl")
 
-export DiffableFunction, BinaryOperation, GradField, Variable, Scalar, AbstractTensor, Tensor, CuTensor, transport!, forward, backward!
+export DiffableFunction, BinaryOperator, UnaryOperator, GradField, Variable, Scalar, AbstractTensor, Tensor, CuTensor, transport!, forward, backward!
 
 end
