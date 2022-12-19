2dd59c1f4f3517d4724d1ccee08064c0e37f08f0
Yuchi Yamaguchi <abap0002@gmail.com>
2022-12-19 17:58:42 +0900

    reimplent promotion

--- src/core/operators.jl
+++ src/core/operators.jl
@@ -1,52 +1,56 @@
 using .AutoDiff
-import .AutoDiff: AutoDiff.forward
+import .AutoDiff: forward, call!
+
 
 import Base
 
-mutable struct Add <: BinaryOperation
+struct Add <: BinaryOperator
     grad_field::GradField
 end
 
-mutable struct Sub <: BinaryOperation
+struct Sub <: BinaryOperator
     grad_field::GradField
 end
 
-mutable struct Neg <: BinaryOperation
+struct Neg <: BinaryOperator
     grad_field::GradField
 end
 
-mutable struct Mul <: BinaryOperation
-    grad_field::GradField
-end
-
-
-mutable struct Div <: BinaryOperation
+struct Mul <: BinaryOperator
     grad_field::GradField
 end
 
 
-mutable struct Pow{T} <: BinaryOperation
+struct Div <: BinaryOperator
+    grad_field::GradField
+end
+
+
+struct Pow{T} <: UnaryOperator
     grad_field::GradField
     c::T
 end
 
 
-@inline AutoDiff.forward(::Add, x1, x2) = x1 + x2
+@inline forward(::Type{Add}, x1, x2) = x1 + x2
 # case: a - b
-@inline AutoDiff.forward(::Sub, x1, x2) = x1 - x2
+@inline forward(::Type{Sub}, x1, x2) = x1 - x2
 # case: -a
-@inline AutoDiff.forward(::Sub, x) = -x
-@inline AutoDiff.forward(::Neg, x) = -x
-@inline AutoDiff.forward(::Mul, x1, x2) = x1 * x2
-@inline AutoDiff.forward(::Div, x1, x2) = x1 / x2
-@inline AutoDiff.forward(f::Pow, x1) = x1^f.c
+@inline forward(::Type{Sub}, x) = -x
+@inline forward(::Type{Neg}, x) = -x
+@inline forward(::Type{Mul}, x1, x2) = x1 * x2
+@inline forward(::Type{Div}, x1, x2) = x1 / x2
+@inline forward(f::Type{Pow}, x1) = x1^f.c
 
+function backward(::Add, gy::R) where R <: Real
+    return (gy, gy)
+end
 
 function backward(::Add, gy::Scalar)
     return (gy, gy)
 end
 
-function backward(::Add, gy::T) where T <: AbstractTensor 
+function backward(::Add, gy::T) where {T<:AbstractTensor}
     @. return (gy, gy)
 end
 
@@ -54,7 +58,7 @@ function backward(::Sub, gy::Scalar)
     return (gy, -gy)
 end
 
-function backward(::Sub, gy::T) where T <: AbstractTensor
+function backward(::Sub, gy::T) where {T<:AbstractTensor}
     return (gy, -gy)
 end
 
@@ -62,7 +66,7 @@ function backward(::Neg, gy::Scalar)
     return -gy
 end
 
-function backward(::Neg, gy::T) where T <: AbstractTensor 
+function backward(::Neg, gy::T) where {T<:AbstractTensor}
     return -gy
 end
 
@@ -71,56 +75,119 @@ function backward(f::Mul, gy::Scalar)
     return (x2 * gy, x1 * gy)
 end
 
-function backward(f::Mul, gy::T) where T <: AbstractTensor
+function backward(f::Mul, gy::T) where {T<:AbstractTensor}
     x1, x2 = f.grad_field.inputs
     @. return (x2 .* gy, x1 .* gy)
 end
 
-function backward(f::Div, gy::Scalar) r
+function backward(f::Div, gy::Scalar)
     x1, x2 = f.grad_field.inputs
     return (1 / x2) * gy, (-x1 / (x2 * x2)) * gy
 end
 
-function backward(f::Div, gy::T) where T <: AbstractTensor
+function backward(f::Div, gy::T) where {T<:AbstractTensor}
     x1, x2 = f.grad_field.inputs
     @. return (1 / x2) * gy, (-x1 / (x2 * x2)) * gy
-end    
+end
 
 
-function backward(f::Pow, gy::Scalar) 
+function backward(f::Pow, gy::Scalar)
     x = f.grad_field.inputs[1]
     c = f.c
     return (c * (x^(c - 1))) * gy
 end
 
 
-function backward(f::Pow, gy::T) where T <: AbstractTensor
+function backward(f::Pow, gy::T) where {T<:AbstractTensor}
     x = f.grad_field.inputs[1]
     c = f.c
     @. return (c * (x^(c - 1))) * gy
 end
 
-const normal_operators = Dict(
-    :+ => Add,
-    :- => Sub,
-    :* => Mul,
-    :/ => Div,
-)
 
-Base.:-(x::T) where T <: Variable = Neg(GradField())(x)
-pure_func(::Neg) = -
-Base.:^(x::Variable, c) = Pow(GradField(), c)(x)
+Base.:+(x1::T, x2::T) where {T <: Scalar} = call!(Add, x1, x2)
+Base.:+(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Add, x1, Scalar(x2))
+Base.:+(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Add, Scalar(x1), x2)
 
-is_support(::typeof(^)) = true
-jt_func(::typeof(^)) = Pow
-pure_func(::Pow) = ^
+Base.:+(x1::T, x2::T) where {T <: Tensor} = call!(Add, x1, x2)
+Base.:+(x1::T, x2::S) where {T <: Tensor, S <: AbstractArray} = call!(Add, x1, Tensor(x2))
+Base.:+(x1::S, x2::T) where {T <: Tensor, S <: AbstractArray} = call!(Add, Tensor(x1), x2)
 
-for (op, jt_func) in normal_operators
-    @eval is_support(::typeof(Base.$op)) = true
-    @eval is_support_broadcast(::typeof(Base.$op)) = true
-    @eval jt_func(::typeof(Base.$op)) = $jt_func
-    @eval pure_func(::$(jt_func)) = Base.$op
-    @eval Base.$op(x1::T, x2::R) where {T <: Variable, R <: Real} = Base.$op(promote(x1, x2)...)
-    @eval Base.$op(x1::R, x2::T) where {T <: Variable, R <: Real} = Base.$op(promote(x1, x2)...)
-    @eval Base.$op(x1::T, x2::S) where {T <: Variable, S <: Variable} = $jt_func(GradField())(x1, x2)
+Base.:+(x1::T, x2::T) where {T <: CuTensor} = call!(Add, x1, x2)
+Base.:+(x1::T, x2::S) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+Base.:+(x1::S, x2::T) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+
+
+
+Base.:-(x1::T, x2::T) where {T <: Scalar} = call!(Sub, x1, x2)
+Base.:-(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Sub, x1, Scalar(x2))
+Base.:-(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Sub, Scalar(x1), x2)
+
+Base.:-(x1::T, x2::T) where {T <: Tensor} = call!(Sub, x1, x2)
+Base.:-(x1::T, x2::S) where {T <: Tensor, S <: AbstractArray} = call!(Sub, x1, Tensor(x2))
+Base.:-(x1::S, x2::T) where {T <: Tensor, S <: AbstractArray} = call!(Sub, Tensor(x1), x2)
+
+Base.:-(x1::T, x2::T) where {T <: CuTensor} = call!(Sub, x1, x2)
+Base.:-(x1::T, x2::S) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+Base.:-(x1::S, x2::T) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+
+
+
+Base.:*(x1::T, x2::T) where {T <: Scalar} = call!(Mul, x1, x2)
+Base.:*(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Mul, x1, Scalar(x2))
+Base.:*(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Mul, Scalar(x1), x2)
+
+Base.:*(x1::T, x2::T) where {T <: Tensor} = call!(Mul, x1, x2)
+Base.:*(x1::T, x2::S) where {T <: Tensor, S <: AbstractArray} = call!(Mul, x1, Tensor(x2))
+Base.:*(x1::S, x2::T) where {T <: Tensor, S <: AbstractArray} = call!(Mul, Tensor(x1), x2)
+
+Base.:*(x1::T, x2::T) where {T <: CuTensor} = call!(Mul, x1, x2)
+Base.:*(x1::T, x2::S) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+Base.:*(x1::S, x2::T) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+
+Base.:*(x1::T, x2::R) where {T <: Tensor, R <: Real} = call!(Mul, x1, Scalar(x2))
+Base.:*(x1::R, x2::T) where {T <: Tensor, R <: Real} = call!(Mul, Scalar(x1), x2)
+
+Base.:*(x1::T, x2::R) where {T <: CuTensor, R <: Real} = call!(Mul, x1, Scalar(x2))
+Base.:*(x1::R, x2::T) where {T <: CuTensor, R <: Real} = call!(Mul, Scalar(x1), x2)
+
+
+
+Base.:/(x1::T, x2::T) where {T <: Scalar} = call!(Div, x1, x2)
+Base.:/(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Div, x1, Scalar(x2))
+Base.:/(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Div, x1, Scalar(x2))
+
+Base.:/(x1::T, x2::T) where {T <: Tensor} = call!(Div, x1, x2)
+Base.:/(x1::T, x2::S) where {T <: Tensor, S <: AbstractArray} = call!(Div, x1, Tensor(x2))
+Base.:/(x1::S, x2::T) where {T <: Tensor, S <: AbstractArray} = call!(Div, Tensor(x1), x2)
+
+Base.:/(x1::T, x2::T) where {T <: CuTensor} = call!(Div, x1, x2)
+Base.:/(x1::T, x2::S) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+Base.:/(x1::S, x2::T) where {T <: CuTensor, S <: AbstractArray} = NotSameDeviceError(same_accelerator=false, same_gpu_idx=false)
+
+Base.:/(x1::T, x2::R) where {T <: Tensor, R <: Real} = call!(Div, x1, Scalar(x2))
+Base.:/(x1::R, x2::T) where {T <: Tensor, R <: Real} = call!(Div, Scalar(x1), x2)
+
+Base.:/(x1::T, x2::R) where {T <: CuTensor, R <: Real} = call!(Div, x1, Scalar(x2))
+Base.:/(x1::R, x2::T) where {T <: CuTensor, R <: Real} = call!(Div, Scalar(x1), x2)
+
+
+function AutoDiff.call!(F::Type{Pow}, x::T, c::R) where {T<:Variable, R <: Real}
+    inputs = (x, )
+    y = x.values ^ c
+    gen = x.generation
+    gf = GradField(
+            inputs,
+            AutoDiff.out_to_tensor(y, gen),
+            gen
+        ) 
+    func = Pow(gf, c)
+    gf.output.creator = func
+    return gf.output
 end
+
+
+Base.:^(x1::T, x2::T) where {T <: Scalar} = call!(Pow, x1, x2.values)
+Base.:^(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Pow, x1, x2)
+Base.:^(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Pow, Scalar(x1), x2.values)
+
