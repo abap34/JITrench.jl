955650b691b7f961b240c5d0b38ff4c78cfaeb1e
Yuchi Yamaguchi <abap0002@gmail.com>
2022-12-29 22:53:44 +0900

    rm unused

--- src/arr/mapfunction.jl
+++ src/arr/mapfunction.jl
@@ -5,11 +5,5 @@ end
 forward(::Type{UnaryMapFunction{F}}, x) where F <: DiffableFunction = forward.(F, x)
 
 function backward(f::UnaryMapFunction{F}, gy::T) where {F, T <: TensorTypes}
-    x = f.grad_field.inputs[1]
     return backward.(Ref(F), gy) 
 end
-
-Base.broadcasted(::typeof(sin), x::Tensor) = call!(UnaryMapFunction{Sin}, x)
-
-
-``
