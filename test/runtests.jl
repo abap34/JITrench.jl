e8f43f33548383cc64cd0db9a30fdf03ecc3f281
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-14 22:55:53 +0900

    add target  files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,29 @@
-using Test
 using JITrench
+using Test
+
+include("./test_utils.jl")
+
+
+test_files = Dict(
+    "autodiff" => [
+        "unary_operators.jl",
+        "binary_operators.jl"
+    ]
+)
+
+
+
+N_test = 100
+
+
+for (dir, files) in test_files
+    @testset "$dir" begin
+        for file in files
+            @testset "$file" begin
+                include(dir * "/" * file)
+            end
+        end
+    end
+end
+
+
