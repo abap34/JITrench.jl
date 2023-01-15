8615c5f1bde1968d75aaccceb089cb3f5077ea92
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-15 21:18:07 +0900

    add export functions

--- src/JITrench.jl
+++ src/JITrench.jl
@@ -8,13 +8,15 @@ include("core/autodiff/autodiff.jl")
 include("core/operators.jl")
 include("core/math_functions.jl")
 
+# begin ArrOperator 
 include("arr/arr.jl")
+# end
 
 include("utils/error.jl")
 include("utils/out.jl")
 include("utils/plot.jl")
 include("utils/utils_macro.jl")
 
-export AutoDiff, backward!
+export AutoDiff, backward!, Scalar, Tensor, CuTensor
 
 end
