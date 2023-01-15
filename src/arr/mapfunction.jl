69411c0f03856ef95abdcc235b47a3eaa7c0dfd2
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-15 21:08:40 +0900

    move some files to core

--- src/arr/mapfunction.jl
+++
@@ -1,9 +0,0 @@
-struct UnaryMapFunction{F} <: UnaryOperator
-    grad_field::GradField
-end
-
-forward(::Type{UnaryMapFunction{F}}, x) where {F <: DiffableFunction} = forward.(F, x)
-
-function backward(f::UnaryMapFunction{F}, gy::T) where {F, T <: TensorTypes}
-    return backward.(Ref(F), gy)
-end
