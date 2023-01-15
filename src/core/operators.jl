6411ac1be99567c67e3e4f50faf759ec0e5a09e5
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-15 21:17:15 +0900

    add support for operations defined between Scalar and Tensor

--- src/core/operators.jl
+++ src/core/operators.jl
@@ -23,13 +23,13 @@ struct Div <: BinaryOperator
     grad_field::GradField
 end
 
-struct PowField{R <: Real} <: AdditionalField
-    c::R
+struct PowField{T} <: AdditionalField
+    c::T
 end
 
-struct Pow{R} <: UnaryOperator
+struct Pow{T} <: UnaryOperator
     grad_field::GradField
-    additional_field::PowField{R}
+    additional_field::PowField{T}
 end
 
 
@@ -61,17 +61,17 @@ end
 
 function backward(f::Mul, gy::TensorTypes) 
     x1, x2 = f.grad_field.inputs
-    @. return (x2 .* gy, x1 .* gy)
+    @. return (x2 * gy, x1 * gy)
 end
 
 function backward(f::Div, gy::ScalarTypes)
     x1, x2 = f.grad_field.inputs
-    return inv(x2) * gy, (-x1 / (x2 * x2)) * gy
+    return inv(x2) * gy, (-x1 / (x2^2)) * gy
 end
 
 function backward(f::Div, gy::TensorTypes) 
     x1, x2 = f.grad_field.inputs
-    @. return inv(x2) * gy, (-x1 / (x2 * x2)) * gy
+    @. return inv(x2) * gy, (-x1 / (x2^2)) * gy
 end
 
 
@@ -93,11 +93,6 @@ Base.:+(x1::Scalar, x2::Scalar) = call!(Add, x1, x2)
 Base.:+(x1::Scalar, x2::Real)   = call!(Add, x1, Scalar(x2))
 Base.:+(x1::Real, x2::Scalar)   = call!(Add, Scalar(x1), x2)
 
-function Base.:+(x1::Tensor, x2::Tensor)
-    check_sameshape(x1, x2)
-    call!(Add, x1, x2)
-end
-
 Base.:+(x1::T, x2::S) where {T <: Tensor, S <: AbstractArray} = call!(Add, x1, Tensor(x2))
 Base.:+(x1::S, x2::T) where {T <: Tensor, S <: AbstractArray} = call!(Add, Tensor(x1), x2)
 
@@ -106,6 +101,41 @@ Base.:+(::T, ::S) where {T <: CuTensor, S <: AbstractArray} =
     NotSameDeviceError(same_accelerator = false, same_gpu_idx = false)
 Base.:+(::S, ::T) where {T <: CuTensor, S <: AbstractArray} =
     NotSameDeviceError(same_accelerator = false, same_gpu_idx = false)
+
+Base.:+(x1::AbstractTensor, x2::Real) = call!(Add, x1, Scalar(x2))
+Base.:+(x1::Real, x2::AbstractArray) = call!(Add, Scalar(x1), x2)
+Base.:+(x1::AbstractArray, x2::Scalar) = call!(Add, Tensor(x1), x2)
+Base.:+(x1::Scalar, x2::AbstractArray) = call!(Add, x1, Tensor(x2))
+
+function Base.:+(x1::AbstractTensor, x2::AbstractTensor)
+    if check_sameshape(x1, x2)
+        call!(Add, x1, x2)
+    else
+        if check_broadcastable(x1, x2)
+            call!(Add, x1, x2)
+        else
+            throw(BroadcastCallError())
+        end
+    end
+end
+
+function Base.:+(x1::AbstractTensor, x2::Scalar)
+    if check_broadcastable(x1, x2)
+        call!(Add, x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
+
+function Base.:+(x1::Scalar, x2::AbstractTensor)
+    if check_broadcastable(x1, x2)
+        call!(Add, x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
 
 
 Base.:-(x1::T, x2::T) where {T <: Scalar} = call!(Sub, x1, x2)
@@ -125,6 +155,40 @@ Base.:-(::S, ::T) where {T <: CuTensor, S <: AbstractArray} =
 Base.:-(x::T) where {T <: Variable} = call!(Neg, x)
 
 
+Base.:-(x1::AbstractTensor, x2::Real) = call!(Sub, x1, Scalar(x2))
+Base.:-(x1::Real, x2::AbstractArray) = call!(Sub, Scalar(x1), x2)
+Base.:-(x1::AbstractArray, x2::Scalar) = call!(Sub, Tensor(x1), x2)
+Base.:-(x1::Scalar, x2::AbstractArray) = call!(Sub, x1, Tensor(x2))
+
+function Base.:-(x1::AbstractTensor, x2::AbstractTensor)
+    if check_sameshape(x1, x2)
+        call!(Sub, x1, x2)
+    else
+        if check_broadcastable(x1, x2)
+            call!(Sub, x1, x2)
+        else
+            throw(BroadcastCallError())
+        end
+    end
+end
+
+function Base.:-(x1::AbstractTensor, x2::Scalar)
+    if check_broadcastable(x1, x2)
+        call!(Sub, x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
+
+function Base.:-(x1::Scalar, x2::AbstractTensor)
+    if check_broadcastable(x1, x2)
+        call!(Sub, x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
 
 Base.:*(x1::T, x2::T) where {T <: Scalar} = call!(Mul, x1, x2)
 Base.:*(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Mul, x1, Scalar(x2))
@@ -140,14 +204,6 @@ Base.:*(::T, ::S) where {T <: CuTensor, S <: AbstractArray} =
 Base.:*(::S, ::T) where {T <: CuTensor, S <: AbstractArray} =
     NotSameDeviceError(same_accelerator = false, same_gpu_idx = false)
 
-Base.:*(x1::T, x2::R) where {T <: Tensor, R <: Real} = call!(Mul, x1, Scalar(x2))
-Base.:*(x1::R, x2::T) where {T <: Tensor, R <: Real} = call!(Mul, Scalar(x1), x2)
-
-Base.:*(x1::T, x2::R) where {T <: CuTensor, R <: Real} = call!(Mul, x1, Scalar(x2))
-Base.:*(x1::R, x2::T) where {T <: CuTensor, R <: Real} = call!(Mul, Scalar(x1), x2)
-
-
-
 Base.:/(x1::T, x2::T) where {T <: Scalar} = call!(Div, x1, x2)
 Base.:/(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Div, x1, Scalar(x2))
 Base.:/(x1::R, x2::T) where {T <: Scalar, R <: Real} = call!(Div, Scalar(x1), x2)
@@ -162,14 +218,42 @@ Base.:/(::T, ::S) where {T <: CuTensor, S <: AbstractArray} =
 Base.:/(::S, ::T) where {T <: CuTensor, S <: AbstractArray} =
     NotSameDeviceError(same_accelerator = false, same_gpu_idx = false)
 
-Base.:/(x1::T, x2::R) where {T <: Tensor, R <: Real} = call!(Div, x1, Scalar(x2))
-Base.:/(x1::R, x2::T) where {T <: Tensor, R <: Real} = call!(Div, Scalar(x1), x2)
-
-Base.:/(x1::T, x2::R) where {T <: CuTensor, R <: Real} = call!(Div, x1, Scalar(x2))
-Base.:/(x1::R, x2::T) where {T <: CuTensor, R <: Real} = call!(Div, Scalar(x1), x2)
-
+function Base.:/(x1::AbstractTensor, x2::AbstractTensor)
+    if check_broadcastable(x1, x2)
+        call!(Div, x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
 
 Base.:^(x1::T, x2::T) where {T <: Scalar} = call!(Pow, PowField(x2.values), x1)
 Base.:^(x1::T, x2::R) where {T <: Scalar, R <: Real} = call!(Pow, PowField(x2), x1)
 Base.:^(x1::R, x2::T) where {T <: Scalar, R <: Real} =
     call!(Pow, PowField(x2.values), Scalar(x1))
+
+
+function Base.:^(x1::AbstractTensor, x2::AbstractTensor)
+    if check_broadcastable(x1, x2)
+        call!(Pow, PowField(x2.values), x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
+function Base.:^(x1::AbstractTensor, x2::Scalar)
+    if check_broadcastable(x1, x2)
+        call!(Pow, PowField(x2), x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
+
+
+function Base.:^(x1::Scalar, x2::AbstractTensor)
+    x1 = Scalar(x1)
+    if check_broadcastable(x1, x2)
+        call!(Pow, PowField(x2), x1, x2)
+    else
+        throw(BroadcastCallError())
+    end
+end
