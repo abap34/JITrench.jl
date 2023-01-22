570edb23643e6d7ec9f8064b9367bf49618d97fd
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-22 16:37:35 +0900

    implement `Fill`

---
+++ src/arr/fill.jl
@@ -0,0 +1,18 @@
+struct FillField{T <: Tuple} <: AdditionalField
+    out_shape ::T
+end
+
+struct Fill{T} <: UnaryOperator
+    grad_field::GradField
+    additional_field::FillField{T}
+end
+
+function forward(::Type{Fill}, additional_field, x)
+    out_shape = additional_field.out_shape
+    return fill(x, out_shape)
+end
+
+function backward(f::Fill, gy)
+    out_shape = f.additional_field.out_shape
+    return out_shape .* gy
+end
