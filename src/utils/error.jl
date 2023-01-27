862e047430602b77785c1bce1192362cfb45e085
Yuchi Yamaguchi <abap0002@gmail.com>
2023-01-28 01:11:41 +0900

    implement `NotSameLengthError`

--- src/utils/error.jl
+++ src/utils/error.jl
@@ -4,16 +4,26 @@ struct NotSameShapeError <: Exception
     shapes
 end
 
-Base.showerror(io::IO, e::NotSameShapeError) = print(io, 
-"Arguments must be same shape, Arguments shaps are $(shape).",
+Base.showerror(io::IO, e::NotSameShapeError) = print(io,
+    "Arguments must be same shape, Arguments shaps are $(shape).",
 )
 
-check_sameshape(args...) = allequal(size.(args)) 
-struct NotImplemetedError <: Exception
-    msg::String
+check_sameshape(args...) = allequal(size.(args))
+
+struct NotSameLengthError <: Exception
+    x_len
+    y_len
 end
 
-Base.showerror(io::IO, e::NotImplemetedError) = print(io, e.msg)
+Base.showerror(io::IO, e::NotSameLengthError) = print(io,
+    "Arguments must be same length. Input length are ($(e.x_len), $(e.y_len)",
+)
+
+function check_samelength(x::AbstractArray, y::AbstractArray)
+    x_len = length(x)
+    y_len = length(y)
+    return x_len != y_len
+end
 
 struct NotSameDeviceError <: Exception
     same_accelerator::Bool
@@ -53,7 +63,7 @@ struct BroadcastCallError <: Exception end
 Base.showerror(io::IO, e::BroadcastCallError) =
     print(io, "Please call this function with `req_broadcast`")
 
-function check_broadcastable(x::T) where {T <: Variable}
+function check_broadcastable(x::T) where {T<:Variable}
     if !(x.req_broadcast)
         throw(BroadcastCallError())
     end
