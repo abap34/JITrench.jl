7303a84c59491e04b4a484f9c68e384cde28aec7
Yuchi Yamaguchi <abap0002@gmail.com>
2023-05-16 16:51:32 +0900

    add include file

--- src/nn/layer/layer.jl
+++ src/nn/layer/layer.jl
@@ -1,4 +1,11 @@
+using ..JITrench 
+
+Parameter = OrderedDict{String, Dict{String, <: Tensor}}
+
 abstract type Layer end
 
+include("./init_distribution.jl")
+include("./parameters.jl")
 include("./initializer.jl")
+include("./compute.jl")
 include("./linear.jl")
