190e1fe2a41e1dcedcef25699cf24cebac14d245
Yuchi Yamaguchi <abap0002@gmail.com>
2023-05-20 21:54:40 +0900

    implement `logsumexp!`

---
+++ src/utils/util_functions.jl
@@ -0,0 +1,15 @@
+function logsumexp!(arr::AbstractArray; dims=1)
+    m = maximum(arr, dims=dims)
+    arr .-= m
+    arr .= exp.(arr)
+    s = sum(arr, dims=dims)
+    s .= log.(s)
+    return s .+ m
+end
+
+function logsumexp!(arr::AbstractArray)
+    m = maximum(arr)
+    arr .-= m
+    arr .= exp.(arr)
+    return log(sum(arr)) + m
+end
