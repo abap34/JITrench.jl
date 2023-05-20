710bca77cd04a6e7665a8d0473e85075d06f3138
Yuchi Yamaguchi <abap0002@gmail.com>
2023-05-20 22:36:00 +0900

    implement `one_hot_batch`

--- src/utils/util_functions.jl
+++ src/utils/util_functions.jl
@@ -1,4 +1,4 @@
-function logsumexp!(arr::AbstractArray, dims=1)
+function logsumexp!(arr::AbstractArray, dims)
     m = maximum(arr, dims=dims)
     arr .-= m
     arr .= exp.(arr)
@@ -12,4 +12,18 @@ function logsumexp!(arr::AbstractArray)
     arr .-= m
     arr .= exp.(arr)
     return log(sum(arr)) + m
+end
+
+
+function one_hot_batch(labels::AbstractArray, nclass::Int)
+    N = size(labels)[1]
+    result = zeros(Bool, N, nclass)
+    for (i, label) in enumerate(labels)
+        result[i, Int(label)] = 1
+    end
+    return result
+end
+
+function one_hot_batch(labels::Tensor, nclass::Int)
+    return Tensor(one_hot_batch(labels.values, nclass))
 end
