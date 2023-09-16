2d46e34620a194d69098e92daa6b84889b81ca6a
abap34 <uchi_ymgc@her0.jp>
2023-09-17 04:27:54 +0900

    add document

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,20 @@
 using JITrench
 using Documenter
 
-DocMeta.setdocmeta!(JITrench, :DocTestSetup, :(using JITrench); recursive = true)
+DocMeta.setdocmeta!(JITrench, :DocTestSetup, :(using JITrench); recursive=true)
 
 makedocs(;
-    modules = [JITrench],
-    authors = "Yuchi Yamaguchi",
-    repo = "https://github.com/abap34/JITrench.jl/blob/{commit}{path}#{line}",
-    sitename = "JITrench.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://abap34.github.io/JITrench.jl",
-        assets = String[],
+    modules=[JITrench],
+    authors="Yuchi Yamaguchi",
+    repo="https://github.com/abap34/JITrench.jl/blob/{commit}{path}#{line}",
+    sitename="JITrench.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://abap34.github.io/JITrench.jl",
+        assets=String[]
     ),
-    pages = ["Home" => "index.md"],
+    pages=["Home" => "index.md",
+        "API" => "api.md",]
 )
 
-deploydocs(; repo = "github.com/abap34/JITrench.jl")
+deploydocs(; repo="github.com/abap34/JITrench.jl")
